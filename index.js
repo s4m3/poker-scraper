259508b983ec7683f82c76eb59ddb166f192aa05
Simon Mary <sma@marketlogicsoftware.com>
2021-12-20 23:38:36 +0100

    send and receive

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ var wss = new WebSocketServer({ server: server })
 console.log("websocket server created")
 
 wss.on("connection", function (ws) {
+  ws.binaryType = 'arraybuffer';
   console.log("websocket connection open");
 
   ws.on("close", function () {
     console.log("websocket connection close");
   })
 
-  ws.on("message", function () {
-    ws.send(JSON.stringify("got it"));
+  ws.on("message", function (data) {
+    console.log('received: %s', data);
+    ws.send(data);
   });
 })
