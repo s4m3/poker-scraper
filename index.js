832cbc855e85bbbbbbdb395987e3345849d63215
Simon Mary <sma@marketlogicsoftware.com>
2021-12-20 22:55:38 +0100

    test send and receive

--- index.js
+++ index.js
@@ -11,18 +11,17 @@ server.listen(port)
 
 console.log("http server listening on %d", port)
 
-var wss = new WebSocketServer({server: server})
+var wss = new WebSocketServer({ server: server })
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+wss.on("connection", function (ws) {
+  console.log("websocket connection open");
 
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
+  ws.on("close", function () {
+    console.log("websocket connection close");
   })
+
+  ws.on("message", function () {
+    ws.send(JSON.stringify("got it"));
+  });
 })
